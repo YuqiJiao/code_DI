ca40e4a7df8ce474b9bcddbdc49a3a2d10ecd708
YuqiJiao <jiaoyuqi984@gmail.com>
2025-01-28 20:25:36 +0800

    Update DIOOB_.py

--- DIOOB_.py
+++ DIOOB_.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 import torch
 import tqdm
@@ -7,15 +6,13 @@ from numpy.random import RandomState
 from opendataval.dataval.api import DataEvaluator
 
 
-
-from partition_ import Partition1, Partition2
-
+from partition_ import Partition
 
 
 
 class DIOOB(DataEvaluator):
 
-    def __init__(self, outer_iter=800, port=0.1, mut=1.7, port_delet=0.3, random_state: RandomState = None, *args,
+    def __init__(self, outer_iter=800, port=0.1, mut=1.7, port_delet=0.3, ratio = 0.02,random_state: RandomState = None, *args,
                  **kwargs):
         super().__init__(*args, **kwargs)
         self.data_val = None
@@ -23,6 +20,7 @@ class DIOOB(DataEvaluator):
         self.port = port
         self.Mut = mut
         self.port_delet = port_delet
+        self.ratio = ratio
         self.random_state = check_random_state(random_state)
 
     def input_data(
@@ -37,7 +35,7 @@ class DIOOB(DataEvaluator):
         self.x_valid = x_valid
         self.y_valid = y_valid
 
-
+        # Additional parameters
         self.pre_num_points = len(x_train)
 
         return self
@@ -51,7 +49,7 @@ class DIOOB(DataEvaluator):
                      ground_truth_clean=None,
                      data_perc=.12,
                      seed=None, device=None,
-                     boost=100,  # 8，alpha
+                     boost=40,  # 8，alpha
                      beta=4,
                      bag=3, *args, **kwargs):
         boost = max(boost, 1)
@@ -62,64 +60,56 @@ class DIOOB(DataEvaluator):
         learners = []
         sets = []
         losses = np.zeros((len(self.x_train),))
+        B = int(1/self.ratio)
         for b in range(1):
             active = active_ == active_
             for e in range(boost):
-                if len(self.y_train[0]) <= 2:
-                    if sum(active)/len(active) <= 0.025:
+                if len(self.x_train) > 800:
+                    if sum(active)/len(active) <= 0.1:
                         break
                 else:
-                    if sum(active)/len(active) <= 0.05:
+                    if sum(active)/len(active) <= 0.001:
                         break
                 classifier_fn = self.pred_model.clone()
 
                 dp = min(1, data_perc * len(active) / sum(active))
-                if len(self.y_train[0]) <= 2:
-                    wl = Partition1(
-                        classifier_fn=classifier_fn,
-                        x_train=self.x_train,
-                        y_train=self.y_train,
-                        active=active,
-                        num_classes=num_classes,
-                        batch_size=batch_size,
-                        data_perc=dp,
-                        refit_it=2 + max(1, min(3, int(1 // dp))),
-                        expansion=1 / beta,
-                        device=device
-                    )
-                    core_set, net = wl.get_core_set()
-                    learners.append(net)
-                    sets.append(core_set)
-                else:
-                    wl = Partition2(
-                        classifier_fn=classifier_fn,
-                        x_train=self.x_train,
-                        y_train=self.y_train,
-                        active=active,
-                        num_classes=num_classes,
-                        batch_size=batch_size,
-                        data_perc=dp,
-                        refit_it=2 + max(1, min(3, int(1 // dp))), b=b, losses=losses,
-                        expansion=1 / beta,
-                        device=device
-                    )
-                    core_set, net, loss = wl.get_core_set()
-                    learners.append(net)
-                    sets.append(core_set)
-                    losses = loss
+                wl = Partition(
+                    # problem set up
+                    classifier_fn=classifier_fn,
+                    #dataset=dataset,
+                    x_train=self.x_train,
+                    y_train=self.y_train,
+                    active=active,
+                    num_classes=num_classes,
+                    batch_size=batch_size,
 
+                    data_perc=dp,
+                    refit_it=2 + max(1, min(3, int(1 // dp))), b=b, losses = losses,
+                    expansion=1 / beta,
+
+
+                    device=device
+                )
+
+                core_set, net, loss = wl.get_core_set()
+                learners.append(net)
+                sets.append(core_set)
+                losses = loss
                 del classifier_fn
 
-                if boost > 1:
+                if boost >= 1:
                     active = np.logical_and(active, ~core_set)
 
                 if active.sum() == 0:
 
                     break
-
+            # boosting
         sets.append(active)
+        setss = np.array(sets)
+        
         c_num = np.zeros(len(learners))
-
+        c_num_ = np.zeros(len(learners))
+        
         v_bool = np.zeros(len(learners))
 
         for idx in range(len(learners)):
@@ -128,7 +118,7 @@ class DIOOB(DataEvaluator):
                 val_y = [self.y_train[i] for i in range(len(self.y_train)) if sets[k][i]]
                 val_x = torch.stack(val_x)
                 val_y = torch.stack(val_y)
-
+                
                 y_pred = learners[idx].predict(val_x)
                 y_pred = torch.Tensor(y_pred)
                 predicted = torch.argmax(y_pred, dim=1)
@@ -140,9 +130,20 @@ class DIOOB(DataEvaluator):
 
                 if sum(predicted==val_y)>len(val_x)*0.5:
                     c_num[idx] += len(val_x)
-        print(c_num)
+                    
+                
+            y_pred_val = learners[idx].predict(self.x_valid)
+            y_pred_val = torch.Tensor(y_pred_val)
+            predicted_val = torch.argmax(y_pred_val, dim=1)
+            predicted_val = predicted_val.cpu().numpy()
+            val_y_ = np.argmax(self.y_valid, axis = 1)
+            val_y_ = val_y_.cpu().numpy()
+            c_num_[idx] = sum(predicted_val == val_y_)
+
+
+
         for idx_ in range(len(learners)):
-            if c_num[idx_] > len(self.x_train)*0.5:
+            if c_num[idx_] >= len(self.x_train)*0.5:
                 v_bool[idx_] = 1
 
         return sets, v_bool
@@ -151,22 +152,31 @@ class DIOOB(DataEvaluator):
 
 
     def train_data_values(self, *args, **kwargs):
+
+
+
         num_class = len(self.y_train[0])
 
         sets, v_bool = self.filter_noise(num_classe=num_class)
 
         bool = v_bool
         bool = np.array(bool)
+
         if np.sum(bool) <= 0.5 * len(bool):
             for i in range(len(bool)):
-                if i >= int(0.5 * len(bool)):
+                if i >= 0.5 * len(bool):
                     v_bool[i] = 1
+
         for i in range(len(v_bool)):
             sets[i] = sets[i] * (1-v_bool[i])
-
+        # sets[-1] = sets[-1]*0
         keep = np.ones(len(self.x_train))
         for i in range(len(v_bool)):
+            # keep = keep + sets[i]
             keep = keep - sets[i]
+
+
+            # #################################################################################################################
         num_keep = np.sum(keep)
         target_y = self.y_train
         target_y = torch.argmax(torch.Tensor(target_y), dim=1)
@@ -207,7 +217,9 @@ class DIOOB(DataEvaluator):
             score = self.evaluate(y_valid, self.y_valid)
             scores[idx] = score
 
-            distance = torch.nn.functional.cross_entropy(y_train_hat, self.y_train[indices_out], reduction='none')
+            distance = torch.nn.functional.cross_entropy(y_train_hat.cpu(), self.y_train[indices_out], reduction='none')
+
+
             distance = distance.detach().cpu().numpy()
 
             distances[idx, indices_out] = distance
